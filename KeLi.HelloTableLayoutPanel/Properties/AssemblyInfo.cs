bea4378ec3790bee310b088b36b61e65d40605f4
KeLi <kelilive@outlook.com>
2022-10-14 00:35:47 +0800

    Update

--- KeLi.HelloTableLayoutPanel/Properties/AssemblyInfo.cs
+++ KeLi.HelloTableLayoutPanel/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeLi.HelloTableLayoutPanel")]
+[assembly: AssemblyTitle("HelloTableLayoutPanel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KeLi.HelloTableLayoutPanel")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("KeLi")]
+[assembly: AssemblyProduct("HelloTableLayoutPanel")]
+[assembly: AssemblyCopyright("Copyright © KeLi 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
